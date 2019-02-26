16d355e7796f740cd3e6983744477ce892c813e5
sh.mirghafari <sh.mirghafari@SUP-LENOVO-E51M>
2019-02-26 14:19:51 +0330

    Somthing Changes!

--- 1/Program.cs
+++ 1/Program.cs
@@ -7,7 +7,25 @@
             //System.Console.WriteLine(" Hello World! ");
             //System.Console.ReadLine();
 
-            System.Console.WriteLine("What is your Name: ");
+            System.Console.Write("What is your first name: ");
+
+            string firstName = System.Console.ReadLine();
+
+            System.Console.Write("What is your last name: ");
+
+            string lastName = System.Console.ReadLine();
+
+            //string fullName = firstName + " " + lastName;
+            //string fullName = 
+            // string.Format("{0} {1}", firstName, lastName);
+            //string fullName = string.Format("FullName: {0} {1}", firstName, lastName);
+            string fullName =
+                $"{firstName} {lastName}";
+
+            System.Console.WriteLine(fullName);
+
+            System.Console.ReadLine();
+
 
 
 
