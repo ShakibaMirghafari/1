e2b1b3d9d9d2dd1e7c1d8aa874525ba3c65f3e13
sh.mirghafari <sh.mirghafari@ghadir.local>
2019-02-26 11:31:49 +0330

    Add project files.

---
+++ 1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Console.WriteLine(" Hello World! ");
+            //System.Console.ReadLine();
+
+            System.Console.WriteLine("What is your Name: ");
+
+
+
+
+        }
+    }
+}
